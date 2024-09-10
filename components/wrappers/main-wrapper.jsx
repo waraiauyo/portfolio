fc0d0923893e52cbef2c36fead803474f4edf0ae
Lixag <valent1.arn@gmail.com>
2024-09-10 23:13:12 +0200

    10/09 3

--- components/wrappers/main-wrapper.jsx
+++ components/wrappers/main-wrapper.jsx
@@ -2,8 +2,15 @@
 
 export default function MainWrapper({children}){
     return(
-        <main className="flex flex-col h-screen gap-6">
-            {children}
-        </main>
+        <>
+            <main
+                className="md:flex flex-col gap-6 hidden">
+                {children}
+            </main>
+            <div className={"md:hidden flex h-screen justify-center items-center p-8"}>
+                <p className={"text-center"}>Pour une meilleur expérience, il est préférable de naviguer sur ce site avec un plus grand écran.</p>
+            </div>
+        </>
+
     )
 }
